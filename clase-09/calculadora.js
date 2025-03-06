39c83557b023eebbbabee7797e25ea721034e50a
Yulo97 <giuliano_devito@hotmail.com>
2025-03-05 22:02:25 -0300

    clase 9

---
+++ clase-09/calculadora.js
@@ -0,0 +1,6 @@
+let num1 = prompt("Ingresa el primer numero")
+let num2 = prompt("Ingresa el segundo numero")
+
+let resultado = parseFloat(num1) + parseFloat(num2)
+
+console.log("Tu resultado es: " + resultado)
