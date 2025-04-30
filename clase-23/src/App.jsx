a892dec4cbb68d87d97fe125d0a28d8923e69be5
Yulo97 <giuliano_devito@hotmail.com>
2025-04-30 18:51:36 -0300

    clase 23

---
+++ clase-23/src/App.jsx
@@ -0,0 +1,24 @@
+import Titulo from "./components/Titulo.jsx"
+import {Tarjeta} from "./components/Tarjeta.jsx"
+
+function App() {
+
+  return (
+    <div>
+      <h1>Esta es mi aplicacion de React</h1>
+
+      <Titulo />
+      <Tarjeta />
+      <Titulo />
+      <Tarjeta />
+      <Titulo />
+      <Tarjeta />
+      <Titulo />
+      <Tarjeta />
+      <Titulo />
+      <Tarjeta />
+    </div>
+  )
+}
+
+export default App
---
+++ clase-23/src/components/Tarjeta.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Tarjeta = () => {
+  return (
+    <div>
+        <h1>ESTA ES LA TARJETA</h1>
+    </div>
+  )
+}
